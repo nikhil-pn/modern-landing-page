ff2e82be7e016971bfae91cf37a6c620931e2451
nikhilpn725 <nikhilpn725@gmail.com>
2023-04-15 01:43:21 +0530

    mobile responsive added

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,26 +12,30 @@ import {
 const Home: NextPage = () => {
   return (
     <motion.div
-      className="min-h-screen px-12 bg-cream"
+      className=" flex flex-col min-h-screen
+      px-4 md:px-12 bg-cream"
       initial="initial"
       animate="animate"
     >
       <Head>
-        <title>Home | Robot Co</title>
+        <title>Home | NtecPhones</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main className="grid grid-cols-[3fr_1fr] py-12">
-        <h1 className="text-8xl font-bold w-[40rem] leading-[90%] tracking-[-2px]">
+      <main className="grid md:grid-cols-[3fr_1fr]  md:py-12">
+        <h1 className=" text-3xl md:text-8xl font-bold md:w-[40rem]   md:leading-[90%] tracking-[-2px] ">
           <AnimatedWords title="Part Of the Journey is the END" />
         </h1>
-        <motion.div
-          className="text-base leading-[150%] self-end"
-          variants={riseWithFade}
-        >
-          Born from the moonshot factory, we're building a new type of robot.
-          One that can learn by itself, to help anyone with (almost) anything.
-        </motion.div>
+        <div className="flex justify-center">
+
+          <motion.div
+            className="text-sm leading-[150%] self-end mt-4 pt-2 justify-center flex items-center "
+            variants={riseWithFade}
+          >
+            Born from the moonshot factory, we're building a new type of robot.
+            One that can learn by itself, to help anyone with (almost) anything.
+          </motion.div>
+        </div>
       </main>
       <motion.video
         className="w-full py-12"
@@ -43,7 +47,24 @@ const Home: NextPage = () => {
       >
         <source src="/assets/hero-video.mp4" type="video/mp4" />
       </motion.video>
-      <footer className="flex justify-center text-sm text-zinc-400 py-12">
+      <div className="flex justify-center">
+
+        <motion.div
+          className="text-sm leading-[150%] self-end mt-4 "
+          variants={riseWithFade}
+        >
+          Born from the moonshot factory, we're building a new type of robot.
+          One that can learn by itself, to help anyone with (almost) anything.
+          Born from the moonshot factory, we're building a new type of robot.
+          One that can learn by itself, to help anyone with (almost) anything.
+
+          One that can learn by itself, to help anyone with (almost) anything.
+          Born from the moonshot factory, we're building a new type of robot.
+          One that can learn by itself, to help anyone with (almost) anything.
+        </motion.div>
+      </div>
+      <footer className="flex justify-center text-sm text-zinc-400 py-12 mt-auto
+">
         <p>Copyright 2022 Robot Co. All rights reserved.</p>
       </footer>
     </motion.div>
